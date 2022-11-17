5eec98804d8b67d6ac2a49a1fd3b926a6aa53da0
Julian <julian.57@outlook.fr>
2022-11-17 03:34:39 +0100

    Create the footer part

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Xp from './components/experience/Xp'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Services />
       <Portfolio />
       <Contact />
+      <Footer />
     </>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {BsYoutube, BsLinkedin} from 'react-icons/bs'
+import {AiOutlineInstagram} from 'react-icons/ai'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#" className='footer_logo'> Initium </a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">A propos</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.youtube.com/channel/UCHvAE1TxZENLhmLb3ZJ9k6g"><BsYoutube/></a>
+        <a href="https://www.instagram.com/real_initium/"><AiOutlineInstagram/></a>
+        <a href="https://www.linkedin.com/in/julian-grimler-a543a0250/"><BsLinkedin/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Initium. Tous droits réservés.</small>
+      </div>
+    </footer>
   )
 }
 
